231e975edc04560bf9f036b21303ff864bcac506
NoeBerdoz <berdoznoe@gmail.com>
2024-09-26 11:58:58 +0200

    it is retro it is cool

---
+++ lua/config/settings.lua
@@ -0,0 +1,53 @@
+-- Disable compatibility with vi which can cause unexpected issues.
+vim.cmd('set nocompatible')
+
+-- Enable filetype detection.
+vim.cmd('filetype on')
+
+-- Enable plugins and load plugin for the detected file type.
+vim.cmd('filetype plugin on')
+
+-- Load an indent file for the detected file type.
+vim.cmd('filetype indent on')
+
+-- Turn syntax highlighting on.
+vim.cmd('syntax on')
+
+-- Add numbers to each line on the left-hand side.
+vim.opt.number = true
+
+-- Set shift width to 4 spaces.
+vim.opt.shiftwidth = 4
+
+-- Set tab width to 4 columns.
+vim.opt.tabstop = 4
+
+-- Use space characters instead of tabs.
+vim.opt.expandtab = true
+
+-- Do not wrap lines. Allow long lines to extend as far as the line goes.
+vim.opt.wrap = false
+
+-- While searching through a file, incrementally highlight matching characters as you type.
+vim.opt.incsearch = true
+
+-- Show partial command you type in the last line of the screen.
+vim.opt.showcmd = true
+
+-- Show matching words during a search.
+vim.opt.showmatch = true
+
+-- Use highlighting when doing a search.
+vim.opt.hlsearch = true
+
+-- Set the commands to save in history; default number is 20.
+vim.opt.history = 1000
+
+-- Enable auto-completion menu after pressing TAB.
+vim.opt.wildmenu = true
+
+-- Make wildmenu behave like Bash completion.
+vim.opt.wildmode = 'list:longest'
+
+-- I don't know i just fix errors
+vim.opt.termguicolors = true
---
+++ lua/plugins/colorsheme.lua
@@ -0,0 +1,42 @@
+-- [[ MIDNIGHT THEME COMMENTED 
+-- return {
+--     "dasupradyumna/midnight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.cmd("colorscheme midnight")
+--     end,
+-- }
+--]]
+
+return {
+    "tiagovla/tokyodark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme tokyodark")
+        vim.o.background = "dark" -- Set background to dark
+    
+        -- Set background color to #000000 for various UI elements
+        local highlights = {
+            Normal = "#000000",
+            NonText = "#000000",
+            LineNr = "#000000",
+            CursorLine = "#000000",
+            SignColumn = "#000000",
+            StatusLine = "#000000",
+            StatusLineNC = "#000000",
+            VertSplit = "#000000",
+            Folded = "#000000",
+            EndOfBuffer = "#000000", -- End of buffer (empty space)
+            Pmenu = "#000000", -- Popup menu
+            PmenuSel = "#FFFFFF", -- Selected item in popup menu (optional)
+            WildMenu = "#000000", -- Wild menu
+        }
+
+        for group, color in pairs(highlights) do
+            vim.cmd(string.format("highlight %s guibg=%s", group, color))
+        end
+    end,
+}
+
